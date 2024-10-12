3c079ec27b1341507ecd324b2cd05d8d944c42d6
Nightmare <fajardoariel435@gmail.com>
2024-10-13 00:38:50 +0200

    sdawd

--- api/controllers/LinkController.js
+++ api/controllers/LinkController.js
@@ -2,7 +2,7 @@ import { z } from 'zod'
 import { prisma } from '../utils/prisma.js'
 
 const urlSchema = z.string().url()
-const API_URL = "https://short-url-azure.vercel.app/url"
+const API_URL = "https://short-url-azure.vercel.app/api/url"
 
 export class LinkController {
     static async index(req, res){
